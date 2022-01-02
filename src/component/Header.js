b5ae60a0482ecd2d9eb26039f44d5db240491af3
Annalartey <74235952+Annalartey@users.noreply.github.com>
2022-01-02 20:31:46 +0000

    initial commit

---
+++ src/component/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className='header space-x-6 lg:space-x-14 lg:text-lg py-8 font-semibold'>
+        <a href="#designs"> <h3>| Dresses</h3> </a>
+        <a href="#about"> <h3>| Bead Work</h3> </a>
+      </div> 
+    )
+}
+
+export default Header
---
+++ src/component/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div className='main text-white'>
+          
+          <div className="text-center my-40 lg:my-56">
+          <h1 className="text-5xl font-bold text-white mb-20">Make Today Amazing... <br/>Wear Us Today</h1>
+          <a href="/" className=" py-1 text-yellow-500 font-semibold border-b-2 border-yellow-500 hover:border-blue-500 transition duration-500 ease-in-out hover:border-b-2 transform hover:-translate-y-1 hover:scale-110 cursor:pointer ">Explore</a>
+        </div>
+          
+        </div>
+    )
+}
+
+export default Home
