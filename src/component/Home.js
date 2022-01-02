44b22c3121422cca2c9daa3982b123f1e389146e
Annalartey <74235952+Annalartey@users.noreply.github.com>
2022-01-02 23:13:30 +0000

    Update Home.js

--- src/component/Home.js
+++ src/component/Home.js
@@ -5,7 +5,7 @@ function Home() {
     return (
         <div className='main text-white'>
           
-          <div className="text-center my-40 lg:my-56">
+          <div className="text-center my-56 lg:my-56">
           <h1 className="text-5xl font-bold text-white md:text-blue">Make Today Amazing...</h1> 
           <br/>
           <h1 className='text-5xl font-bold text-white mb-20 md:text-blue'>Wear Us Today</h1>
