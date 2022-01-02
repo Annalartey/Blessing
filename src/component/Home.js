df7bdacba111b32d7c03ea922b9634a74701ef95
Annalartey <74235952+Annalartey@users.noreply.github.com>
2022-01-02 21:34:50 +0000

    update

--- src/component/Home.js
+++ src/component/Home.js
@@ -5,8 +5,10 @@ function Home() {
         <div className='main text-white'>
           
           <div className="text-center my-40 lg:my-56">
-          <h1 className="text-5xl font-bold text-white mb-20">Make Today Amazing... <br/>Wear Us Today</h1>
-          <a href="/" className=" py-1 text-yellow-500 font-semibold border-b-2 border-yellow-500 hover:border-blue-500 transition duration-500 ease-in-out hover:border-b-2 transform hover:-translate-y-1 hover:scale-110 cursor:pointer ">Explore</a>
+          <h1 className="text-5xl font-bold text-white">Make Today Amazing...</h1> 
+          <br/>
+          <h1 className='text-5xl font-bold text-white mb-20'>Wear Us Today</h1>
+          <a href="/" className=" py-1 text-yellow-500 font-bold border-b-2 border-yellow-500 hover:text-white hover:border-white transition duration-500 ease-in-out hover:border-b-2 transform hover:-translate-y-1 hover:scale-110 cursor:pointer ">Explore</a>
         </div>
           
         </div>
